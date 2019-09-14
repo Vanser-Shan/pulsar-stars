6380d848747f305bb2ee73688d5640e30d08cf0a
Vanser-Shan <adrian.physicist@gmail.com>
2019-09-14 21:17:45 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 14 20:53:03 2019
+
+@author: aaranda
+"""
+import numpy as np
+import pandas as pd
+
+
+dataset = pd.read_csv('pulsar_stars.csv')
